aa1f7a65162366dc507d9973ac1078685864789d
Chili <shaolj2@lenovo.com>
2026-08-05 15:26:09 +0800

    使用pydantic model_validate_json校验json输入

--- day-01/main.py
+++ day-01/main.py
@@ -1,4 +1,63 @@
 from fastapi import FastAPI
+from pydantic import BaseModel,ValidationError,EmailStr,Field
+from typing import Optional
+from datetime import date
+
+#定义用户输入模型
+class UserInput(BaseModel):
+    name: str
+    email: EmailStr #pydanic会自动校验邮箱格式
+    query: str
+    orderid: Optional[int] = Field(
+        None,
+        description="5-digit order number(cannot start with 0)",
+        ge = 10000,
+        le = 99999
+    )
+    purchase_date: Optional[date] = None
+#Creat model instance
+# user_input = UserInput(
+#     name = "Joe User",
+#     email="1024747899@qq.com",
+#     query="I Forgot my password"
+# )    
+# print(user_input)
+
+def validate_user_input(input_data):
+    try:
+        user_input = UserInput(**input_data)
+        print(f"🤓 Vaild User Input Created:")
+        print(f"{user_input.model_dump_json(indent=2)}")
+        return user_input
+    except ValidationError as e:
+        print(f"😒 Validation error occurred:")
+        for error in e.errors():
+            print("  - {error['loc'][0]: {error['msg']}")
+        return None
+    
+
+# input_data = {
+#     "name" : "Joe User",
+#     "email":"1024747899@qq.com",
+#     "query":"I Forgot my password",
+#     "orderid": "12345",
+#     "purchase_date": date(2026,8,5) 
+# }
+#模拟用户输入内容由LLM 整理出json
+json_data = '''
+{
+    "name" : "Joe User",
+    "email":"1024747899@qq.com",
+    "query":"I Forgot my password",
+    "orderid": "12345",
+    "purchase_date": "2026-08-05" 
+}
+'''
+input_data = UserInput.model_validate_json(json_data)
+print(input_data.model_dump_json(indent=2))
+# user_input = validate_user_input(input_data)
+
+exit()    
 
 app = FastAPI() #创建API实例
 print("111")
